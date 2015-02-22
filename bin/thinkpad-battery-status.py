4a8d0f30c3b1a9f29bec24cd2e226961dde566b3
Nick Morrott <knowledgejunkie@gmail.com>
2015-02-22 03:51:29 +0000

    Battery status: Unbreak prompt when no battery is found. Other tidying.

--- bin/thinkpad-battery-status.py
+++ bin/thinkpad-battery-status.py
@@ -17,46 +17,48 @@ PATH='/sys/class/power_supply/BAT0/uevent'
 rem, full, state, percentage = 10.0, 10.0, 'No battery', 10
 
 if path.isfile(PATH) and access(PATH, R_OK):
-    rem = float(commands.getoutput("grep ^POWER_SUPPLY_ENERGY_NOW= " + PATH + " | tr '=' ' ' | awk '{ print $2 }'"))
-    full = float(commands.getoutput("grep ^POWER_SUPPLY_ENERGY_FULL= " + PATH + " | tr '=' ' ' | awk '{ print $2 }'"))
+    rem   = float(commands.getoutput("grep ^POWER_SUPPLY_ENERGY_NOW= "  + PATH + " | tr '=' ' ' | awk '{ print $2 }'"))
+    full  = float(commands.getoutput("grep ^POWER_SUPPLY_ENERGY_FULL= " + PATH + " | tr '=' ' ' | awk '{ print $2 }'"))
     state = commands.getoutput("cat /sys/class/power_supply/BAT0/status")
+else:
+    sys.stdout.write('')
+    sys.exit()
 
 percentage = int((rem/full) * 10)
 
 # Output
 
 total_slots, slots = 10, []
-#filled = int(math.ceil(percentage * (total_slots / 10.0))) * u'▸'
 filled = int(math.ceil(percentage * (total_slots / 10.0))) * u'▶'
-#empty = (total_slots - len(filled)) * u'▹'
-empty = (total_slots - len(filled)) * u'▷'
-#charge =  (u'↯').encode('utf-8')
-charge =  (u'⌁').encode('utf-8')
-#charged =  (u'✓').encode('utf-8')
-charged =  (u'⌁').encode('utf-8')
-out = (filled + empty).encode('utf-8')
+empty  = (total_slots - len(filled)) * u'▷'
+out    = (filled + empty).encode('utf-8')
 
-color_green = '%{[32m%}'
+charging =  (u'↯').encode('utf-8')
+charged  =  (u'✓').encode('utf-8')
+
+color_green  = '%{[32m%}'
 color_yellow = '%{[1;33m%}'
-color_red = '%{[31m%}'
-color_reset = '%{[00m%}'
+color_red    = '%{[31m%}'
+color_reset  = '%{[00m%}'
+
 color_out = (
-    color_green if len(filled) > 6
-    else color_yellow if len(filled) > 4
+    color_green if len(filled) > 4
+    else color_yellow if len(filled) > 2
     else color_red
 )
-inb = color_yellow + charge + color_reset
-inb2 = color_green + charged + color_reset
-out = color_out + out + color_reset
+
+charging    = color_yellow + charging + color_reset
+charged     = color_green  + charged  + color_reset
+discharging = color_out    + out      + color_reset
 
 # Omit battery status if we can't find the required /sys node
 if state == "No battery":
-    sys.stdout.write(color_reset)
+    sys.stdout.write('')
 elif state == "Full":
-    sys.stdout.write(inb2)
+    sys.stdout.write(charged)
 elif state == "Unknown":
-    sys.stdout.write(inb2)
+    sys.stdout.write('')
 elif state == "Charging":
-    sys.stdout.write(inb)
+    sys.stdout.write(charging)
 else:
-    sys.stdout.write(out)
+    sys.stdout.write(discharging)
